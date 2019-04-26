0316f918b293d0237df6dadd9da1c4b40aaa57b0
Jungyeon Yoon <jungyeon.yoon@gatech.edu>
2019-04-26 11:57:25 -0400

    add Makefile

--- mytest/reader.c
+++ mytest/reader.c
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc == 1) {
+        printf("give some path\n");
+        return 1;
+    }
+
     PMEMobjpool *pop = pmemobj_open(argv[1], LAYOUT_NAME);
     if (pop == NULL) {
         perror("pmemobj_open");
@@ -14,10 +19,10 @@ int main(int argc, char *argv[])
     PMEMoid root = pmemobj_root(pop, sizeof(struct my_root));
     struct my_root *rootp = pmemobj_direct(root);
 
-    char buf[MAX_BUF_LEN];
-    scanf("%9s", buf);
+    char my_buf[MAX_BUF_LEN];
+    scanf("%9s", my_buf);
 
-    rootp->len = strlen(buf);
+    rootp->len = strlen(my_buf);
     pmemobj_persist(pop, &rootp->len, sizeof(rootp->len));
     pmemobj_memcpy_persist(pop, rootp->buf, my_buf, rootp->len);
 
--- mytest/writer.c
+++ mytest/writer.c
@@ -5,12 +5,20 @@
 
 int main(int argc, char *argv[])
 {
+    if ( argc == 1 ) {
+        printf("give some path\n");
+        return 1;
+    }
+
     PMEMobjpool *pop = pmemobj_create(argv[1], LAYOUT_NAME, PMEMOBJ_MIN_POOL, 0666);
     if ( pop == NULL ) {
         perror("pmemobj_create");
         return 1;
     }
 
+    char buf[MAX_BUF_LEN] = "nice try..";
+
+    strcpy(pop, buf);
 
     pmemobj_close(pop);
 
